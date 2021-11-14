c7028ba0dbf4af8feacd717f3807ebadd157390d
vt <kidal5@centrum.cz>
2021-11-14 18:45:48 +0100

    added possibility to override default place for parameters file

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ from sheet_times import createSheetTimes
 from sheet_number_of_travels import createSheetNumberOfTravels
 from sheet_number_of_cars import createSheetNumberOfCars
 
+import argparse
+
+def parseInput():
+    parser = argparse.ArgumentParser(description='Helper script, that reads excel data, processes them in pandas and writes them back into excel.')
+    parser.add_argument('-p', '--paramsFile',  type=str, help='Optional path for parameters file. Defaulted to ./parameters.yaml', default='parameters.yaml', required=False)
+
+    arguments = parser.parse_args()
+    arguments = vars(arguments)
+    return arguments
+
 if __name__ == '__main__':
-    with open('parameters.yaml') as f:
+    args = parseInput()
+    with open(args['paramsFile']) as f:
         params = yaml.safe_load(f)
 
     print('Loading data')
