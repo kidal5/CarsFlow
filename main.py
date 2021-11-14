1d0d456d245663f2a8e8fd480ef34a0f39aad2f0
vt <kidal5@centrum.cz>
2021-11-14 18:53:10 +0100

    added generating folders, when needed for output file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yaml
 import os
 import os.path as osp
 import pandas as pd
+from pathlib import Path
 
 from load_utils import loadExcel
 from sheet_times import createSheetTimes
@@ -10,13 +11,18 @@ from sheet_number_of_cars import createSheetNumberOfCars
 
 import argparse
 
+
 def parseInput():
-    parser = argparse.ArgumentParser(description='Helper script, that reads excel data, processes them in pandas and writes them back into excel.')
-    parser.add_argument('-p', '--paramsFile',  type=str, help='Optional path for parameters file. Defaulted to ./parameters.yaml', default='parameters.yaml', required=False)
+    parser = argparse.ArgumentParser(
+        description='Helper script, that reads excel data, processes them in pandas and writes them back into excel.')
+    parser.add_argument('-p', '--paramsFile', type=str,
+                        help='Optional path for parameters file. Defaulted to ./parameters.yaml',
+                        default='parameters.yaml', required=False)
 
     arguments = parser.parse_args()
     arguments = vars(arguments)
     return arguments
+
 
 if __name__ == '__main__':
     args = parseInput()
@@ -26,6 +32,8 @@ if __name__ == '__main__':
     print('Loading data')
     df = loadExcel(params['input_file'], params['number_of_cameras'])
 
+    print('Delete previous output file, if present.')
+    os.makedirs(Path(params['output_file']).parent, exist_ok=True)
     if osp.isfile(params['output_file']):
         os.remove(params['output_file'])
 
@@ -40,5 +48,3 @@ if __name__ == '__main__':
     print('Creating sheet Časové údaje')
     createSheetTimes(df.copy(True), writer, params)
     writer.close()
-
-
